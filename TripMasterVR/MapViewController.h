9cb7661c4ce015b307ed3af730a79add9cb5213e
rozularen <markcosta15@hotmail.com>
2016-10-06 12:40:23 +0200

    añadido libreria y probando

--- TripMasterVR/MapViewController.h
+++ TripMasterVR/MapViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SWRevealViewController.h"
 
 @interface MapViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 @end
