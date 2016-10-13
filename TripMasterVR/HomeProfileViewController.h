20db2a6f781c50b5f4826d9ebeb36dc8c1059778
Marcos Stival Tomé <marcos.stival@macalmpu1034.local>
2016-10-13 16:19:01 +0200

    Ya no hay tab bar controller. Menu lateral funciona. Api funciona. Pin funciona

---
+++ TripMasterVR/HomeProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeProfileViewController.h
+//  TripMasterVR
+//
+//  Created by Marcos Stival Tomé on 11/10/16.
+//  Copyright © 2016 Pablo Rojas Cifre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeProfileViewController : UIViewController
+
+@end
